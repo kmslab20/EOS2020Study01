751af0a91dbdb13b935db8df802ff6165a0edbb8
kkgs0 <kkgs0324@outlook.com>
2020-08-19 16:03:18 +0900

    Lecture02 commit

--- app/src/main/java/com/example/eos2020study01/MainActivity.kt
+++ app/src/main/java/com/example/eos2020study01/MainActivity.kt
@@ -2,10 +2,18 @@ package com.example.eos2020study01
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        button01.setOnClickListener {
+            //change the text in textView01
+            textView01.text = "Button Clicked"
+        }
+
+
     }
 }
